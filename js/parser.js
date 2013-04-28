b02c95fab32e86e3d4169881bb3756264ea3dc21
Chris Barr <chris.m.barr@gmail.com>
2013-04-27 19:51:24 -0500

    Simplifying parser function layout

--- js/parser.js
+++ js/parser.js
@@ -5,67 +5,66 @@ var parser = (function(){
 	var $errorContainer;
 	var $outputSelection;
 
-	var setup={
-		pageInit:function(){
-			//Fill in the variables
-			$dropArea = $("#drop-area");
-			$errorContainer = $("#error-display");
-			$outputSelection = $("#js-output");
+	function _init(){
+		//Fill in the variables
+		$dropArea = $("#drop-area");
+		$errorContainer = $("#error-display");
+		$outputSelection = $("#js-output");
 
-			if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile|Windows Phone|Zune/i.test(navigator.userAgent) ) {
-				displayError("Sorry, SongParserJS this cannot be used on mobile devices!");
-				$dropArea.hide();
-				$outputSelection.hide();
-			}else if(Modernizr.draganddrop && window.FileReader){
-				//If the browser does not nativly support Base64 encode/decode,
-				//load in a file to add this support
-				Modernizr.load({
-					test: window.atob && window.btoa,
-					nope: "js/base64.js"
-				});
-
-				//Init the drag-n-drop feature
-				setup.fileDragAndDrop();
-			}else{
-				//no drag-n-drop support
-				displayError("Sorry, you won't be able to use SongParserJS<br />because your browser does not support file Drag-N-Drop!<br/><br/>Try using a modern browser like <a href='www.google.com/chrome'>Google Chrome</a> instead!");
-				$dropArea.hide();
-				$outputSelection.hide();
-			}
-		},
-		fileDragAndDrop:function(){
-			$dropArea.fileDragAndDrop(function(data, fullFileName){
-				//Empty out the UI so we can put in new data...
-				$errorContainer.empty().hide();
-
-				try{
-
-					//Find the file extention
-					var fileParts = fullFileName.split(".");
-					var fileName = fileParts[0];
-					var fileExt = fileParts.slice(-1)[0].toLowerCase();
-
-					//Make sure the file extention matches up with an existing parser
-					if($.isFunction(parser.formats[fileExt])){
-						//Browsers will add some unneeded text to the base64 encoding. Remove it.
-						var encodedSongData = data.replace(/^data:.*;base64,/,"");
-						var decodedSongData = utilities.decode(encodedSongData);
-
-						//Pass the decoded song date to the parser
-						//We will get back a normalized version of the song content for any supported file type
-						var normalizedSongData = parser.formats[fileExt](decodedSongData, fileName);
-
-						//Pass the final song data to the selected output type
-						parser.outputs[$outputSelection.val()](normalizedSongData);
-					}else{
-						displayError("The file <strong>"+fullFileName+"</strong> cannot be parsed because <strong>."+fileExt.toUpperCase()+"</strong> is not a supported file type!")
-					}
-				}catch(ex){
-					displayError("There was an error reading the file <strong>"+fullFileName+"</strong>");
-				}
+		if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile|Windows Phone|Zune/i.test(navigator.userAgent) ) {
+			displayError("Sorry, SongParserJS this cannot be used on mobile devices!");
+			$dropArea.hide();
+			$outputSelection.hide();
+		}else if(Modernizr.draganddrop && window.FileReader){
+			//If the browser does not nativly support Base64 encode/decode,
+			//load in a file to add this support
+			Modernizr.load({
+				test: window.atob && window.btoa,
+				nope: "js/base64.js"
 			});
+
+			//Init the drag-n-drop feature
+			_fileDragAndDrop();
+		}else{
+			//no drag-n-drop support
+			displayError("Sorry, you won't be able to use SongParserJS<br />because your browser does not support file Drag-N-Drop!<br/><br/>Try using a modern browser like <a href='www.google.com/chrome'>Google Chrome</a> instead!");
+			$dropArea.hide();
+			$outputSelection.hide();
 		}
-	};
+	}
+
+	function _fileDragAndDrop(){
+		$dropArea.fileDragAndDrop(function(data, fullFileName){
+			//Empty out the UI so we can put in new data...
+			$errorContainer.empty().hide();
+
+			try{
+
+				//Find the file extention
+				var fileParts = fullFileName.split(".");
+				var fileName = fileParts[0];
+				var fileExt = fileParts.slice(-1)[0].toLowerCase();
+
+				//Make sure the file extention matches up with an existing parser
+				if($.isFunction(parser.formats[fileExt])){
+					//Browsers will add some unneeded text to the base64 encoding. Remove it.
+					var encodedSongData = data.replace(/^data:.*;base64,/,"");
+					var decodedSongData = utilities.decode(encodedSongData);
+
+					//Pass the decoded song date to the parser
+					//We will get back a normalized version of the song content for any supported file type
+					var normalizedSongData = parser.formats[fileExt](decodedSongData, fileName);
+
+					//Pass the final song data to the selected output type
+					parser.outputs[$outputSelection.val()](normalizedSongData);
+				}else{
+					displayError("The file <strong>"+fullFileName+"</strong> cannot be parsed because <strong>."+fileExt.toUpperCase()+"</strong> is not a supported file type!")
+				}
+			}catch(ex){
+				displayError("There was an error reading the file <strong>"+fullFileName+"</strong>");
+			}
+		});
+	}
 	
 	//==========================
 	//PUBLIC FUNCTIONS BELOW
@@ -91,7 +90,7 @@ var parser = (function(){
 	//==========================
 	//PAGE LOAD
 	//==========================
-	$(setup.pageInit);
+	$(_init);
 
 	return {
 		utilities: utilities,
