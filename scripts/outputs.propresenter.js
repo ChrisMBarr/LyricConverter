ba3e59ecacf18df7d15821f9251a95ed83aaab0a
Chris Barr <chris.m.barr@gmail.com>
2013-04-29 22:43:12 -0500

    After converting to ProPresenter format, option given to download as a ZIP archive or invividual files

--- scripts/outputs.propresenter.js
+++ scripts/outputs.propresenter.js
@@ -8,11 +8,26 @@
 	//Extend the outputs object on the parser to allow for HTML output
 	parser.outputs[THIS_OUTPUT] = function ($container, songList) {
 
+		//Loop through and convert each file and add the 
+		var convertedFileContents=[];
+		var errorFiles = [];
 		$.each(songList, function(i, song) {
-			var ppDoc = _makeProPresenterFile(song.data);
-			var blob = new Blob([ppDoc], {type: "text/xml;charset=utf-8"});
-			saveAs(blob, song.name+".pro4");
-		})
+			try{
+				convertedFileContents.push({
+					name: song.name,
+					data: _makeProPresenterFile(song.data)
+				});
+			}catch(ex){
+				errorFiles.push(song.name);
+			}
+		});
+
+		//Display any errors if we have them
+		if(errorFiles.length){
+			parser.displayError(errorFiles.join(", "), "Error converting the following " + errorFiles.length + " songs!");
+		}
+
+		_displaySuccessHtml($container, convertedFileContents);
 	}
 
 	//===================================
@@ -120,5 +135,50 @@
 		'</RVDisplaySlide>'].join("\n");
 
 		return slideXML;
+	}
+
+	function _displaySuccessHtml($container, convertedFileContents){
+		//Display any successes if we have them
+		if(convertedFileContents.length > 0){
+			//Make some unique ID's we can select on later
+			var downloadZipId = "btn-"+THIS_OUTPUT+"-download-zip";
+			var downloadFilesId = "btn-"+THIS_OUTPUT+"-download-files";
+
+			//Build up some HTML to write out
+			var finalHtml = "<h1>Converted " + convertedFileContents.length + " Song File" + (convertedFileContents.length>1?"s":"") + "!</h1>";
+			if(convertedFileContents.length > 1){
+				finalHtml += "<button id='"+downloadZipId+"' type='button' class='btn btn-large btn-primary'>Download as .zip</button>";	
+				finalHtml += " or <button id='"+downloadFilesId+"' type='button' class='btn'>Download "+convertedFileContents.length + " individual files</button>";
+			}else{
+				finalHtml += "<button id='"+downloadFilesId+"' type='button' class='btn btn-large btn-primary'>Download file</button>";
+			}
+			
+			//Write the HTML to the page
+			$container.html(finalHtml);
+
+			//Add click events to each button we added above
+			$("#"+downloadZipId).on("click",function(){
+				
+				//Create a new .ZIP archive
+				var zip = new JSZip();
+
+				//Go through and add each fiel to the zip
+				$.each(convertedFileContents, function(i, convertedSong) {
+					zip.file(convertedSong.name+".pro4", convertedSong.data)
+				});
+
+				//Generate the zip file contents
+				var zipContent = zip.generate({type:"blob"});
+				//Download it!
+				saveAs(zipContent, "converted files.zip");
+			});
+
+			$("#"+downloadFilesId).on("click",function(){
+				$.each(convertedFileContents, function(i, convertedSong) {
+					var fileBlob = new Blob([convertedSong.data], {type: "text/xml;charset=utf-8"});
+					saveAs(fileBlob, convertedSong.name+".pro4");
+				});
+			});
+		}
 	}
 })();
