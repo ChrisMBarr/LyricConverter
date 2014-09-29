1716809af87d0c9f1171e5bb11df2cac4bb49ec7
Chris Barr <chris.m.barr@gmail.com>
2014-09-28 21:54:59 -0400

    Formatting

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,7 +1,7 @@
 /*!
  * LICENSE:
  * CC BY-NC-SA 3.0
- * This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * This work is licensed under a Creative Commons Attribution - NonCommercial - ShareAlike 3.0 Unported License.
  * http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
  */
 
@@ -19,57 +19,57 @@ var parser = (function() {
         encode: function(str) {
             return window.btoa(unescape(encodeURIComponent(str)));
         }
-    }
+    };
 
-        function parseFile(i, fileObj) {
-            var data = fileObj.data;
-            var fullFileName = fileObj.name;
+    function parseFile(i, fileObj) {
+        var data = fileObj.data;
+        var fullFileName = fileObj.name;
 
-            try {
-                //Find the file extention
-                var fileParts = fullFileName.split(".");
-                var fileExt = fileParts.slice(-1)[0].toLowerCase();
-                var fileName = fullFileName.replace(fileExt, '');
+        try {
+            //Find the file extention
+            var fileParts = fullFileName.split(".");
+            var fileExt = fileParts.slice(-1)[0].toLowerCase();
+            var fileName = fullFileName.replace(fileExt, '');
 
-                //Browsers will add some unneeded text to the base64 encoding. Remove it.
-                var encodedSongData = data.replace(/^data:.*;base64,/, "");
-                var decodedSongData = utilities.decode(encodedSongData);
+            //Browsers will add some unneeded text to the base64 encoding. Remove it.
+            var encodedSongData = data.replace(/^data:.*;base64,/, "");
+            var decodedSongData = utilities.decode(encodedSongData);
 
 
-                //Test the file extension with the test function registered with each format type
-                //When one matches, use that formats convert function
-                var convertFn;
-                $.each(parser.formats, function(format, formatObj) {
-                    if (formatObj.testFormat(fileExt, decodedSongData)) {
-                        convertFn = formatObj.convert;
-                        return false;
-                    }
+            //Test the file extension with the test function registered with each format type
+            //When one matches, use that formats convert function
+            var convertFn;
+            $.each(parser.formats, function(format, formatObj) {
+                if (formatObj.testFormat(fileExt, decodedSongData)) {
+                    convertFn = formatObj.convert;
+                    return false;
+                }
+            });
+
+            //Make sure the convert function exists...
+            if ($.isFunction(convertFn)) {
+                //Pass the decoded song date to the convert function
+                //We will get back a normalized version of the song content for the supported file type
+                parser.songList.push({
+                    name: fileName,
+                    data: convertFn(decodedSongData, fileName)
                 });
 
-                //Make sure the convert function exists...
-                if ($.isFunction(convertFn)) {
-                    //Pass the decoded song date to the convert function
-                    //We will get back a normalized version of the song content for the supported file type
-                    parser.songList.push({
-                        name: fileName,
-                        data: convertFn(decodedSongData, fileName)
-                    });
-
-                } else {
-                    parser.errorList.push("The file <strong>" + fullFileName + "</strong> cannot be parsed because <strong>." + fileExt.toUpperCase() + "</strong> files are not supported!")
-                }
-            } catch (ex) {
-                console.error(ex);
-                parser.errorList.push("There was an error reading the file <strong>" + fullFileName + "</strong>");
+            } else {
+                parser.errorList.push("The file <strong>" + fullFileName + "</strong> cannot be parsed because <strong>." + fileExt.toUpperCase() + "</strong> files are not supported!")
             }
+        } catch (ex) {
+            console.error(ex);
+            parser.errorList.push("There was an error reading the file <strong>" + fullFileName + "</strong>");
         }
+    }
 
-        function complete($outputContainer) {
-            if (parser.songList.length) {
-                //Pass the final song data to the selected output type
-                parser.outputs[parser.outputFormat]($outputContainer, parser.songList);
-            }
+    function complete($outputContainer) {
+        if (parser.songList.length) {
+            //Pass the final song data to the selected output type
+            parser.outputs[parser.outputFormat]($outputContainer, parser.songList);
         }
+    }
 
     return {
         //Expose functions publicly
