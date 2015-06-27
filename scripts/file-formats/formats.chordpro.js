2a74c3f673edc2363497befe0967600e474e5f20
Chris Barr <chris.m.barr@gmail.com>
2015-06-27 12:11:19 -0400

    Re-deploying, but without the partial OpemLyrics support yet

--- scripts/file-formats/formats.chordpro.js
+++ scripts/file-formats/formats.chordpro.js
@@ -1,3 +1,10 @@
+/* global parser */
+/*!
+ * LICENSE:
+ * CC BY-NC-SA 3.0
+ * This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
+ */
 /*=====================================================
  * PARSER for ChordPro/Chord files
  * Extension: .txt
@@ -32,7 +39,7 @@
     //PRIVATE FUNCTIONS
     //===================================
 
-    var _infoRegex = new RegExp(/{.+?:.+?}/gmi);
+    var _infoRegex = /{.+?:.+?}/gmi;
 
     function _getSongMetadata(songData, fileName) {
         var infoArr = [];
