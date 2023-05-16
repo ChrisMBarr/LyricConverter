c864d8a63ac9200228e502fbcfdeea3cef2854c3
FiniteLooper <chris.m.barr@gmail.com>
2023-05-15 23:54:11 -0400

    Show a dynamic file type support table

--- src/app/help/help.component.spec.ts
+++ src/app/help/help.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelpComponent } from './help.component';
+import { ParserService } from '../convert/parser/parser.service';
+import { By } from '@angular/platform-browser';
 
 describe('HelpComponent', () => {
   let component: HelpComponent;
   let fixture: ComponentFixture<HelpComponent>;
 
+  const mockParserService = {
+    inputConverters: [{ name: 'Foo' }, { name: 'Bar' }, { name: 'InputOnly' }],
+    outputConverters: [{ name: 'Foo' }, { name: 'Bar' }, { name: 'OutputOnly' }],
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HelpComponent],
+      providers: [{ provide: ParserService, useValue: mockParserService }],
     });
     fixture = TestBed.createComponent(HelpComponent);
     component = fixture.componentInstance;
@@ -17,5 +25,66 @@ describe('HelpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should build a supported formats list of all inputs and outputs from the ParserService', () => {
+    expect(component.combinedFormatsList).toEqual([
+      {
+        name: 'Foo',
+        canImport: true,
+        canExport: true,
+      },
+      {
+        name: 'Bar',
+        canImport: true,
+        canExport: true,
+      },
+      {
+        name: 'InputOnly',
+        canImport: true,
+        canExport: false,
+      },
+      {
+        name: 'OutputOnly',
+        canImport: false,
+        canExport: true,
+      },
+    ]);
+  });
+
+  it('should display a table in the UI to match the supported formats list', () => {
+    expect(fixture.debugElement.queryAll(By.css('table tbody tr')).length).toEqual(4);
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(1) td:nth-of-type(1)'))
+        .nativeElement.textContent
+    ).toEqual('✓');
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(1) td:nth-of-type(2)'))
+        .nativeElement.textContent
+    ).toEqual('✓');
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(2) td:nth-of-type(1)'))
+        .nativeElement.textContent
+    ).toEqual('✓');
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(2) td:nth-of-type(2)'))
+        .nativeElement.textContent
+    ).toEqual('✓');
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(3) td:nth-of-type(1)'))
+        .nativeElement.textContent
+    ).toEqual('✓');
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(3) td:nth-of-type(2)'))
+        .nativeElement.textContent
+    ).toEqual('');
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(4) td:nth-of-type(1)'))
+        .nativeElement.textContent
+    ).toEqual('');
+    expect(
+      fixture.debugElement.query(By.css('table tbody tr:nth-of-type(4) td:nth-of-type(2)'))
+        .nativeElement.textContent
+    ).toEqual('✓');
   });
 });
--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParserService } from '../convert/parser/parser.service';
+
+interface ICombinedFormatItem {
+  name: string;
+  canImport: boolean;
+  canExport: boolean;
+}
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
-  styleUrls: ['./help.component.scss']
+  styleUrls: ['./help.component.scss'],
 })
-export class HelpComponent {
+export class HelpComponent implements OnInit {
+  combinedFormatsList: ICombinedFormatItem[] = [];
 
+  constructor(private parserSvc: ParserService) {}
+
+  ngOnInit(): void {
+    //Add all input/import types to the list
+    this.parserSvc.inputConverters
+      .map((t) => t.name)
+      .forEach((t) => {
+        this.combinedFormatsList.push({ name: t, canImport: true, canExport: false });
+      });
+
+    //Add all output/export types to the list, but exclude 'Display Slides'
+    //If the name matches one that's already in the list from the inputs/imports
+    //being added then just change a value to flag it as also allowing output/export
+    this.parserSvc.outputConverters
+      .filter((t) => t.name !== 'Display Slides')
+      .map((t) => t.name)
+      .forEach((t) => {
+        const matchedToInput = this.combinedFormatsList.find((inputType) => inputType.name === t);
+        if (matchedToInput) {
+          matchedToInput.canExport = true;
+        } else {
+          this.combinedFormatsList.push({ name: t, canImport: false, canExport: true });
+        }
+      });
+  }
 }
