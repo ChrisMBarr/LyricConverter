7952553958cf7d2fe98314d77d7db6df4641b3b4
FiniteLooper <chris.m.barr@gmail.com>
2023-05-16 00:21:36 -0400

    Adding missing tests for LyricConverter JSON input

--- src/app/convert/inputs/input-type-lyric-converter.spec.ts
+++ src/app/convert/inputs/input-type-lyric-converter.spec.ts
@@ -16,9 +16,9 @@ describe('InputTypeLyricConverter', () => {
     const testFile: IRawDataFile = {
       name: 'foo',
       ext: 'json',
-      type:'text/json',
-      data: '{}'
-    }
+      type: 'text/json',
+      data: '{}',
+    };
     expect(inputConverter.doesInputFileMatchThisType(testFile)).toBeTrue();
   });
 
@@ -26,9 +26,9 @@ describe('InputTypeLyricConverter', () => {
     const testFile: IRawDataFile = {
       name: 'foo',
       ext: 'txt',
-      type:'text/plain',
-      data: 'this is some plain text'
-    }
+      type: 'text/plain',
+      data: 'this is some plain text',
+    };
     expect(inputConverter.doesInputFileMatchThisType(testFile)).toBeFalse();
   });
 
@@ -36,9 +36,56 @@ describe('InputTypeLyricConverter', () => {
     const testFile: IRawDataFile = {
       name: 'foo',
       ext: 'pro5',
-      type:'',
-      data: '<RVPresentationDocument height="768" width="1024" versionNumber="400" docType="0"></RVPresentationDocument>'
-    }
+      type: '',
+      data: '<RVPresentationDocument height="768" width="1024" versionNumber="400" docType="0"></RVPresentationDocument>',
+    };
     expect(inputConverter.doesInputFileMatchThisType(testFile)).toBeFalse();
+  });
+
+  it('should return a song when extractSongData() is called', () => {
+    const testFile: IRawDataFile = {
+      name: 'foo',
+      ext: 'json',
+      type: 'text/json',
+      data: '{"title":"Great is your faithfulness O God","info":[{"name":"Order","value":"1C2CBC"}],"slides":[{"title":"Chorus","lyrics":"Your grace is enough\\r\\nYour grace is enough\\r\\nYour grace is enough for me"},{"title":"Verse 1","lyrics":"Great is your faithfulness O God\\r\\nYou wrestle with the sinners heart\\r\\nYou lead us by still waters and to mercy\\r\\nAnd nothing can keep us apart"}]}',
+    };
+
+    expect(inputConverter.extractSongData(testFile)).toEqual({
+      fileName: testFile.name,
+      title: 'Great is your faithfulness O God',
+      info: [
+        {
+          name: 'Order',
+          value: '1C2CBC',
+        },
+      ],
+      slides: [
+        {
+          title: 'Chorus',
+          lyrics: 'Your grace is enough\r\nYour grace is enough\r\nYour grace is enough for me',
+        },
+        {
+          title: 'Verse 1',
+          lyrics:
+            'Great is your faithfulness O God\r\nYou wrestle with the sinners heart\r\nYou lead us by still waters and to mercy\r\nAnd nothing can keep us apart',
+        },
+      ],
+    });
+  });
+
+  it('should return an empty song when bad data is passed to extractSongData()', () => {
+    const testFile: IRawDataFile = {
+      name: 'foo',
+      ext: 'json',
+      type: 'text/json',
+      data: 'bad data',
+    };
+
+    expect(inputConverter.extractSongData(testFile)).toEqual({
+      fileName: testFile.name,
+      title: '',
+      info: [],
+      slides: [],
+    });
   });
 });
