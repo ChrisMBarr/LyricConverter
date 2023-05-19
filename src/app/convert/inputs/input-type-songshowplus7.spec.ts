34aee16b8d1c4a0c42f6d10ce4f625a533214649
FiniteLooper <chris.m.barr@gmail.com>
2023-05-19 14:48:25 -0400

    SongShowPlus parsing! Should work the same as the previous version of LyricConverter now

--- src/app/convert/inputs/input-type-songshowplus7.spec.ts
+++ src/app/convert/inputs/input-type-songshowplus7.spec.ts
@@ -7,7 +7,14 @@ import {
 import { TestUtils } from 'test/test-utils';
 import { IRawDataFile } from '../models/file.model';
 import { InputTypeSongShowPlus7 } from './input-type-songshowplus7';
-import { mockSongShowPlusFile1 } from 'test/mock-songshowplus7-files';
+import {
+  mockSongShowPlusFile1,
+  mockSongShowPlusFile2,
+  mockSongShowPlusFile3,
+  mockSongShowPlusFile4,
+  mockSongShowPlusSpanishFile1,
+  mockSongShowPlusSpanishFile2,
+} from 'test/mock-songshowplus7-files';
 
 describe('InputTypeSongShowPlus7', () => {
   let inputConverter: InputTypeSongShowPlus7;
@@ -42,15 +49,261 @@ describe('InputTypeSongShowPlus7', () => {
     });
   });
 
-  fdescribe('extractSongData()', () => {
-    it('should return a song for a SongShow Plus 7 file1', () => {
+  describe('extractSongData()', () => {
+    it('should return a song for an ENGLISH SongShow Plus 7 file1', () => {
       const testFile: IRawDataFile = TestUtils.deepClone(mockSongShowPlusFile1);
 
       expect(inputConverter.extractSongData(testFile)).toEqual({
         fileName: testFile.name,
-        title: '',
-        info: [],
-        slides: []
+        title: 'Be Near',
+        info: [
+          { name: 'Artist/Author', value: 'Barnard, Shane' },
+          { name: 'Copyright', value: '2003 Waiting Room Music' },
+          { name: 'CCLI', value: '4090362' },
+          { name: 'Keywords', value: "Longing, Security'" },
+        ],
+        slides: [
+          {
+            title: 'Chorus 1',
+            lyrics:
+              'Be near O God\r\nBe near O God of us\r\nYour nearness is to us our good\r\nBe near O God\r\nBe near O God of us\r\nYour nearness is to us our good\r\nOur good',
+          },
+          {
+            title: 'Other',
+            lyrics: '',
+          },
+          {
+            title: 'Verse 1',
+            lyrics:
+              "oYou are all big and small\r\nBeautiful\r\nAnd wonderful\r\nTo trust in grace through faith\r\nBut I'm asking to taste",
+          },
+          {
+            title: 'Verse 2',
+            lyrics:
+              'eFor dark is light to You\r\nDepths are height to You\r\nFar is near\r\nBut Lord I need to hear from You',
+          },
+          {
+            title: 'Verse 3',
+            lyrics:
+              'pYour fullness is mine\r\nRevelation divine\r\nBut oh to taste\r\nTo know much more than a page\r\nTo feel Your embrace',
+          },
+          {
+            title: 'Verse 4',
+            lyrics:
+              'cFor dark is light to You\r\nDepths are height to You\r\nFar is near\r\nBut Lord I need to hear from You',
+          },
+          {
+            title: 'Ending',
+            lyrics: 'My good\r\n',
+          },
+        ],
+      });
+    });
+
+    it('should return a song for an ENGLISH SongShow Plus 7 file2', () => {
+      const testFile: IRawDataFile = TestUtils.deepClone(mockSongShowPlusFile2);
+
+      expect(inputConverter.extractSongData(testFile)).toEqual({
+        fileName: testFile.name,
+        title: 'Give Us Clean Hands',
+        info: [
+          {
+            name: 'Artist/Author',
+            value: 'Hall, Charlie',
+          },
+          {
+            name: 'Copyright',
+            value: '2000 worshiptogether.com songs',
+          },
+          {
+            name: 'CCLI',
+            value: '2060208',
+          },
+          {
+            name: 'Keywords',
+            value: 'Prayer, \nRepentance',
+          },
+        ],
+        slides: [
+          {
+            title: 'Chorus 1',
+            lyrics:
+              'Give us clean hands\r\ngive us pure hearts\r\nLet us not lift our\r\nsouls to another\r\nGive us clean hands\r\ngive us pure hearts\r\nLet us not lift our\r\nsouls to another',
+          },
+          {
+            title: 'Chorus',
+            lyrics:
+              'And oh God let us be\r\na generation that seeks\r\nThat seeks Your face\r\noh God of Jacob\r\nAnd oh God let us be\r\na generation that seeks\r\nThat seeks Your face\r\noh God of Jacob',
+          },
+          {
+            title: 'Verse 1',
+            lyrics:
+              'We bow our hearts\r\nwe bend our knees\r\nOh Spirit come\r\nmake us humble\r\nWe turn our eyes\r\nfrom evil things\r\nOh Lord we cast\r\ndown our idols\r\n',
+          },
+        ],
+      });
+    });
+
+    it('should return a song for an ENGLISH SongShow Plus 7 file3', () => {
+      const testFile: IRawDataFile = TestUtils.deepClone(mockSongShowPlusFile3);
+
+      expect(inputConverter.extractSongData(testFile)).toEqual({
+        fileName: testFile.name,
+        title: 'Jesus Saves (2)',
+        info: [
+          {
+            name: 'Artist/Author',
+            value: 'Eddie James',
+          },
+          {
+            name: 'Copyright',
+            value: 'Â© Fresh Wine Publishing',
+          },
+        ],
+        slides: [
+          {
+            title: 'Verse 1',
+            lyrics:
+              'Jesus saves from the power\r\nAnd the penalty of sin\r\nJesus saves from the torment\r\nAnd the anguish within\r\nJesus saves from the bondage\r\nAnd control of the enemy\r\nHe heals the broken heart\r\nAnd set the captives free',
+          },
+          {
+            title: 'Verse 2',
+            lyrics:
+              "Jesus saves from the guilt\r\nAnd the shame of what you've done\r\nJesus saves from a life\r\nFilled with lies and deception\r\nJesus saves from the pain of the memories \r\nThat have scarred your past",
+          },
+          {
+            title: 'Pre-chorus 1',
+            lyrics:
+              'Gabriel came to Joseph in a dream\r\nFor by the Holy Spirit Mary conceived\r\nAnd with a message from heaven\r\nGabriel came declaring',
+          },
+          {
+            title: 'Chorus 1',
+            lyrics:
+              'She shall bring forth a Son thou\r\nShalt call His name Jesus and He shall save\r\nSave His people from sin and give peace within\r\nWhat amazing grace - Jesus saves',
+          },
+          {
+            title: 'Verse 3r',
+            lyrics:
+              "Jesus saves, He's the One that has never known sin\r\nJesus saves, yet for us He took on the sins of men\r\nJesus saves for he became our sin that we \r\nMay become the righteousness of God\r\nJesus saves and now I'll see His face in peace\r\nJesus saves and I will live with Him eternally\r\nJesus saves and I will join with the angels\r\nAround the throne Singing Holy, Holy, Holy",
+          },
+          {
+            title: 'Verse 4z',
+            lyrics:
+              "Jesus saves and today you can leave here set free\r\nJesus saves and even now He will give you liberty\r\nJesus saves no matter what, no matter who, no\r\nMatter where just call Him, He'll answer your prayer\r\nJesus saves and your life will never be the same\r\nJesus saves and right now you can be born again\r\nJesus saves for he that be in Christ\r\nThe old has past and all is made new",
+          },
+          {
+            title: 'Bridge 1',
+            lyrics:
+              "Redeemer, liberator, healer, emancipator \r\nChain-Breaker, strong deliverer, He fights for me\r\nWarrior, restorer, forgiver, peace make\r\nJustifier, Intercessor, through Him I'm free",
+          },
+          {
+            title: 'Vamp 1',
+            lyrics: "Jesus saves (repeat) Hallelujah (repeat)\r\n'(",
+          },
+        ],
+      });
+    });
+
+    it('should return a song for an ENGLISH SongShow Plus 7 file4', () => {
+      const testFile: IRawDataFile = TestUtils.deepClone(mockSongShowPlusFile4);
+
+      expect(inputConverter.extractSongData(testFile)).toEqual({
+        fileName: testFile.name,
+        title: 'You Are (2)',
+        info: [
+          {
+            name: 'Artist/Author',
+            value: '8Jobe, Caleb | Cohen, Ezra | Hesami, Josh | Trimble, Paul',
+          },
+          {
+            name: 'Copyright',
+            value: '2010 CFN Music',
+          },
+          {
+            name: 'CCLI',
+            value: '5715921',
+          },
+          {
+            name: 'Keywords',
+            value: "Appreciation, Breakthrough, Christ, Declaration, Jesus'",
+          },
+        ],
+        slides: [
+          {
+            title: 'Verse 1',
+            lyrics:
+              "Bought back from a life in chains\r\nNow I can sing that I have been redeemed\r\nYour blood covers all of me\r\nI'm not ashamed to shout who You are\r\nI'm living in the light of grace\r\nMercy made a way to make things bright\r\nLove overcame the grave in victory in victory",
+          },
+          {
+            title: 'Chorus 1',
+            lyrics:
+              'You are You are You are the Lord of all\r\nGave Your life to save us\r\nFreedom has embraced us\r\nYou are You are You are the Love for all\r\nHope of every nation\r\nAll creation shouts Your glory',
+          },
+          {
+            title: 'Verse 2',
+            lyrics:
+              "I'm running with a heart that's changed\r\nEverything to see Your kingdom come\r\nYou guide every step I take\r\nI'm not ashamed to shout who You are\r\nI'm living in a brand new day\r\nA life to give a life to lift You high\r\nAll glory and power and praise\r\nTo You alone to You alone",
+          },
+          {
+            title: 'Bridge',
+            lyrics:
+              "I'm not ashamed of who You are\r\nYour love broke through\r\nAnd grace has made me Yours\r\nNow upon this Rock I stand\r\nIn victory in victory\r\n(REPEAT 3X)\r\n",
+          },
+        ],
+      });
+    });
+
+    it('should return a song for a SPANISH SongShow Plus 7 file1', () => {
+      const testFile: IRawDataFile = TestUtils.deepClone(mockSongShowPlusSpanishFile1);
+
+      expect(inputConverter.extractSongData(testFile)).toEqual({
+        fileName: testFile.name,
+        title: 'Devuelveme El Gozo',
+        info: [
+          {
+            name: 'Keywords',
+            value: 'Tahoma, ?',
+          },
+        ],
+        slides: [
+          {
+            title: 'Verse 1©',
+            lyrics:
+              'En medio del dolor \r\nEn medio de la aflicciÃ³n\r\nTu me das paz y \r\nMe enseÃ±as tu amor \r\nTodo lo que perdÃ­ \r\nLo restauras SeÃ±or en mi \r\nMe das las fuerza para seguir',
+          },
+          {
+            title: 'Chorus',
+            lyrics:
+              'Devuelve me el gozo \r\nDe Tu salvaciÃ³n y\r\nTu EspÃ­ritu noble me sustente \r\nTe necesito Dios \r\nSin ti no soy nada \r\nTe necesito Dios \r\nDame un nuevo corazÃ³n\r\n"',
+          },
+        ],
+      });
+    });
+
+    it('should return a song for a SPANISH SongShow Plus 7 file2', () => {
+      const testFile: IRawDataFile = TestUtils.deepClone(mockSongShowPlusSpanishFile2);
+
+      expect(inputConverter.extractSongData(testFile)).toEqual({
+        fileName: testFile.name,
+        title: 'La Sangre (The Blood)',
+        info: [
+          {
+            name: 'Keywords',
+            value: 'Background Improv',
+          },
+        ],
+        slides: [
+          {
+            title: 'Verse 1',
+            lyrics:
+              'La sangre de mi Cristo\r\nque el vertio por mi en la cruz\r\naun es eficaz, para limpiar tu ser\r\nporque Cristo nunca perdera su fuerza',
+          },
+          {
+            title: 'Chorus',
+            lyrics:
+              'Oh, porque alcanza a limpiar nuestras manchas\r\ny alcanza a curar nuestras llagas\r\npecador ven al manantial \r\nque fluyendo esta\r\ny lavara tu ser\r\nporque Cristo nunca perdera su fuerza!!"',
+          },
+        ],
       });
     });
   });
--- src/app/convert/inputs/input-type-songshowplus7.ts
+++ src/app/convert/inputs/input-type-songshowplus7.ts
@@ -1,5 +1,6 @@
 import { IRawDataFile } from '../models/file.model';
 import { ISong, ISongInfo, ISongSlide } from '../models/song.model';
+import { TextCleaner } from '../utils/text-cleaner';
 import { IInputConverter } from './input-converter.model';
 
 export class InputTypeSongShowPlus7 implements IInputConverter {
@@ -11,17 +12,270 @@ export class InputTypeSongShowPlus7 implements IInputConverter {
   }
 
   extractSongData(rawFile: IRawDataFile): ISong {
-    const title = rawFile.name; //default/fallback name
-    const info: ISongInfo[] = [];
-    const slides: ISongSlide[] = [];
+    let title = rawFile.name; //default/fallback name
+    let info: ISongInfo[] = [];
+    let slides: ISongSlide[] = [];
 
-    console.log(rawFile)
+    //We don't want any properties XML tags which can sometimes begin the file.
+    //Splitting these out and then taking the first array item can prevent this.
+    //Each song sections seems to be split up by a percent sign, so make an array by splitting on that
+    const propSections = rawFile.data.split('<Properties>');
+    if (propSections[0]) {
+      const sections = propSections[0].split('%');
+
+      if (sections) {
+        //Pass all the sections in here to get the lyrics
+        //We will get out the slides and the keywords
+        const slideContent = this.getSlidesAndKeywords(sections);
+        slides = slideContent.slides;
+
+        if (sections[0]) {
+          //The info is all contained in the first section, so only pass that in and pass in the keywords from above
+          const parsedInfo = this.getTitleAndInfo(sections[0], slideContent.keywords, rawFile.name);
+          title = parsedInfo.title;
+          info = parsedInfo.info;
+        }
+      }
+    }
 
     return {
       fileName: rawFile.name,
       title,
       info,
       slides,
+    };
+  }
+
+  //Regex pattern AS A STRING to match invisible control characters
+  //Slashes are double escaped here so it can be in a string!
+  private readonly patternInvisibleCharsStr = '[\\xA0\\x00-\\x09\\x0B\\x0C\\x0E-\\x1F\\x7F]';
+  //Same pattern, but as a real RexExp object
+  private readonly patternInvisibleChars = new RegExp(this.patternInvisibleCharsStr, 'g');
+
+  private getTitleAndInfo(
+    firstSection: string,
+    keywords: string,
+    fileName: string
+  ): { title: string; info: ISongInfo[] } {
+    //Split the info up into an array by the invisible characters
+    //Then remove all empty items and items that are only 1 character long
+    const infoArray = firstSection
+      .split(this.patternInvisibleChars)
+      .filter((n) => n.trim().replace(/\r\n\t/g, '').length > 1);
+
+    let songTitle = fileName; //Fallback title
+
+    if (infoArray && infoArray[0]) {
+      //If the first items is a number between 1 and 4 digits, remove it
+      if (/[0-9]{1,4}/.test(infoArray[0])) {
+        infoArray.splice(0, 1);
+      }
+
+      songTitle = infoArray[0];
+
+      //remove leading dollar signs for whatever reason...
+      songTitle =
+        songTitle.indexOf('$') === 1 ? songTitle.substring(1, songTitle.length) : songTitle;
+
+      //remove trailing dollar signs for whatever reason...
+      songTitle = songTitle.substring(
+        0,
+        songTitle.length - +(songTitle.lastIndexOf('$') === songTitle.length - 1)
+      );
+    }
+
+    //Convert characters as needed - useful for non-English alphabets (Spanish)
+    songTitle = TextCleaner.convertWin1252ToUtf8(songTitle);
+
+    return {
+      info: this.getSongInfo(infoArray, keywords),
+      title: songTitle,
+    };
+  }
+
+  private getSongInfo(infoArray: string[], keywords: string): ISongInfo[]{
+    const songInfo = [];
+
+    if (infoArray[1]) {
+      songInfo.push({
+        name: 'Artist/Author',
+        value: infoArray[1].trim(),
+      });
+    }
+
+    //If the copyright exists, add it
+    if (infoArray[2]) {
+      songInfo.push({
+        name: 'Copyright',
+        value: infoArray[2].replace('$', '').trim(), //copyright info tends to end with a $ sign, so remove it
+      });
+    }
+
+    //If the CCLI exists, add it
+    if (infoArray[3]) {
+      songInfo.push({
+        name: 'CCLI',
+        value: infoArray[3].trim(),
+      });
+    }
+
+    //If we have keywords, add them
+    if (keywords) {
+      songInfo.push({
+        name: 'Keywords',
+        value: keywords.trim(),
+      });
+    }
+
+    for (const info of songInfo) {
+      info.value = TextCleaner.convertWin1252ToUtf8(info.value);
+    }
+
+    return songInfo;
+  }
+
+  private createInitialSlidesArray(sections: string[]): ISongSlide[]{
+    const slideArray = [];
+
+    //Sections tend to begin with N number of control characters, a random print character, more control characters, and then the title "Verse 1" or something
+    //After that is the actual song lyrics, but it may be proceeded by one non-word character
+    //Slashes are double escaped here so it can be in a string!
+    const slidePattern = new RegExp(
+      '^' +
+        this.patternInvisibleCharsStr +
+        '+.{1}' +
+        this.patternInvisibleCharsStr +
+        '+(.+)' +
+        this.patternInvisibleCharsStr +
+        '+\\W*([\\s\\S]+)',
+      'm'
+    );
+
+    //Loop through the sections
+    //But SKIP the first one since it contains the song info we don't need here
+    for (let i = 1; i < sections.length; i++) {
+      const thisSection = sections[i] || '';
+      //Run the regex on each section to split out the slide title from the lyrics
+      const matches = thisSection.match(slidePattern);
+      let slideTitle = '';
+      let slideLyrics = '';
+
+      //Remove whitespace from the title
+      if (matches) {
+        if (matches[1]) {
+          slideTitle = matches[1].replace(this.patternInvisibleChars, '').trim();
+        }
+        if (matches[2]) {
+          //Remove any more invisible chars from the lyrics and remove whitespace
+          slideLyrics = matches[2].replace(this.patternInvisibleChars, '').trim();
+        }
+      }
+
+      //Convert characters as needed - useful for non-English alphabets (Spanish)
+      slideTitle = TextCleaner.convertWin1252ToUtf8(slideTitle);
+      slideLyrics = TextCleaner.convertWin1252ToUtf8(slideLyrics);
+
+      //Replace multiple slashes sometimes?
+      //Also remove some strange ugly characters...
+      slideLyrics = slideLyrics.replace(/\/+|¶/g, '');
+
+      //Save it to the array!
+      slideArray.push({
+        title: slideTitle,
+        lyrics: slideLyrics,
+      });
+    }
+
+    return slideArray;
+  }
+
+  private getSlidesAndKeywords(sections: string[]): { slides: ISongSlide[]; keywords: string } {
+    const slideArray = this.createInitialSlidesArray(sections)
+
+    //The last slide also contains the keywords, we need to parse these out separately
+    const lastSlideObj = this.getKeywordsFromLastSlide(sections.slice(-1)[0]);
+    let keywords = '';
+    if (lastSlideObj.lastLyrics !== '') {
+      //If we have no slides, and what we think are keywords are longer than the lyrics...
+      //Then we might need to switch them for some reason...
+      if (
+        slideArray.length === 0 &&
+        lastSlideObj.keywords.length > lastSlideObj.lastLyrics.length
+      ) {
+        keywords = lastSlideObj.lastLyrics;
+        slideArray.push({
+          title: '',
+          lyrics: lastSlideObj.keywords.replace(/\/+/g, ''),
+        });
+      } else {
+        keywords = lastSlideObj.keywords;
+        if (slideArray.length > 0) {
+          const lastSlide = slideArray.slice(-1)[0];
+          if (lastSlide) {
+            lastSlide.lyrics = lastSlideObj.lastLyrics;
+          }
+        } else {
+          slideArray.push({
+            title: '',
+            lyrics: lastSlideObj.lastLyrics.replace(/\/+/g, ''),
+          });
+        }
+      }
+    }
+
+    //Only add it if the title and the lyrics don't match. Sometimes they do for some reason...
+    const finalArray = [];
+    for (const s of slideArray) {
+      if (s.title.trim().toLowerCase() !== s.lyrics.trim().toLowerCase()) {
+        finalArray.push(s);
+      }
+    }
+
+    return {
+      slides: finalArray,
+      keywords: keywords,
+    };
+  }
+
+  private getKeywordsFromLastSlide(lastSlideRaw: string | undefined): {
+    keywords: string;
+    lastLyrics: string;
+  } {
+    let keywords = '';
+    let lastLyrics = '';
+
+    if (lastSlideRaw) {
+      //Remove all empty items and items that are only 1 character long
+      const infoArray = lastSlideRaw
+        .split(this.patternInvisibleChars)
+        .filter((n: string) => n.trim().length > 1);
+
+      //If we have at least 3 sections, then we have keywords
+      if (infoArray.length > 2) {
+        //The keywords are the entire array except for the first two items
+        keywords = infoArray
+          .splice(2)
+          .join(', ')
+          .replace(/\r\n\t/g, '');
+
+        if (infoArray && infoArray[1]) {
+          //Return the last slide minus the keywords, then parse out the optional beginning non-word character
+          const lastSlideNonWordsRemoved = infoArray[1].match(/^\W*([\s\S]+)/m);
+
+          if (lastSlideNonWordsRemoved && lastSlideNonWordsRemoved[1]) {
+            lastLyrics = lastSlideNonWordsRemoved[1];
+          }
+        }
+
+        //Convert characters as needed - useful for non-english alphabets (Spanish)
+        keywords = TextCleaner.convertWin1252ToUtf8(keywords);
+        lastLyrics = TextCleaner.convertWin1252ToUtf8(lastLyrics);
+      }
+    }
+
+    return {
+      keywords,
+      lastLyrics,
     };
   }
 }
