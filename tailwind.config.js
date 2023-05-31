c2441c5120c401741130c3d2c6b096a86ee8a4ec
FiniteLooper <chris.m.barr@gmail.com>
2023-05-30 23:48:06 -0400

    Getting tailwind basics in place

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      // '2xl': '1536px',
+    },
+    extend: {
+      backgroundImage: {
+        "lc-1": "url('./assets/bg1.jpg')",
+      },
+      colors: {
+        "lc-highlight": "#3ea8b2",
+      },
+    },
   },
   plugins: [],
 };
