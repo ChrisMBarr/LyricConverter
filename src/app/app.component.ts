e92ec9597e435b7294bbc2ef29f61d14ffe36ac4
FiniteLooper <chris.m.barr@gmail.com>
2023-06-01 14:57:07 -0400

    Multiple randomized BG images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly backgroundImagesCount = 8;
+  randomBackgroundImagePath = `/assets/bg1.jpg`; //default just in case
   title = 'Lyric Converter';
+
+  ngOnInit(): void {
+    const randomNum = Math.floor(Math.random() * this.backgroundImagesCount + 1);
+    this.randomBackgroundImagePath = `/assets/bg${randomNum}.jpg`;
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "lc-1": "url('./assets/bg1.jpg')",
-      },
       colors: {
         "lc-highlight": "#3ea8b2", //hsl(185.17deg 48.33% 47.06%);
         "lc-light": "#7bc9d1", //    hsl(185.17deg 48.33% 65%); - a lighter version of the highlight color
