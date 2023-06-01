3194f95b16b3cc4de8bdc11de7542876d62a4178
FiniteLooper <chris.m.barr@gmail.com>
2023-06-01 13:40:46 -0400

    Slide display improvements & more slides per line on larger screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      // '2xl': '1536px',
-    },
     extend: {
       backgroundImage: {
         "lc-1": "url('./assets/bg1.jpg')",
