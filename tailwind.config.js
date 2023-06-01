e508d929fb3066a3edc5cc9f75fbb9771962d455
FiniteLooper <chris.m.barr@gmail.com>
2023-06-01 00:36:59 -0400

    Improve improve nav for mobile, and layout of convert page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
       // '2xl': '1536px',
     },
     extend: {
@@ -14,7 +14,8 @@ module.exports = {
         "lc-1": "url('./assets/bg1.jpg')",
       },
       colors: {
-        "lc-highlight": "#3ea8b2",
+        "lc-highlight": "#3ea8b2", //hsl(185.17deg 48.33% 47.06%);
+        "lc-light": "#7bc9d1", //    hsl(185.17deg 48.33% 65%); - a lighter version of the highlight color
       },
     },
   },
