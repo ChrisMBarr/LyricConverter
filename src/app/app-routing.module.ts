7225a5295541a7280a0f98308e8afa3445942a1b
Chris Barr <chris.m.barr@gmail.com>
2025-09-13 17:00:34 -0400

    Package + Angular updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,11 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, {
-      initialNavigation: 'enabledBlocking',
-    }),
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
