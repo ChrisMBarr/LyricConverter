976fa6cddc515678cf7e4994fdde1f111a4d6a33
Chris Barr <chris.m.barr@gmail.com>
2025-10-03 17:14:27 -0400

    package updates and minor lint fixes

--- src/main.server.ts
+++ src/main.server.ts
@@ -4,6 +4,7 @@ import { bootstrapApplication, BootstrapContext } from '@angular/platform-browse
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> => bootstrapApplication(AppComponent, config, context);
+const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> =>
+  bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
