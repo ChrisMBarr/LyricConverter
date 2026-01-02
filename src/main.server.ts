e0d3d6b0e6dceaf4f62d3781f3b76e8c136c0f2a
Chris Barr <chris.m.barr@gmail.com>
2026-01-02 12:24:08 -0500

    Update to angular v21 & other packages too

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,10 +1,14 @@
-import { ApplicationRef } from '@angular/core';
+import { ApplicationRef, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
 const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> =>
-  bootstrapApplication(AppComponent, config, context);
+  bootstrapApplication(
+    AppComponent,
+    { ...config, providers: [provideZoneChangeDetection(), ...config.providers] },
+    context,
+  );
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { Buffer } from 'buffer';
 
@@ -6,6 +7,9 @@ import { appConfig } from './app/app.config';
 
 window.Buffer = Buffer; // Needed for SongShowPlus-Parser
 
-bootstrapApplication(AppComponent, appConfig).catch((err: unknown) => {
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((err: unknown) => {
   console.error(err);
 });
